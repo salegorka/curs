7f4a2aece5635b6762e8bd28c94486d6ad803f52
egor_salomatin <salomatin.e@introvert.bz>
2022-06-03 20:46:58 +0500

    recycler view для категорий товаров

--- app/src/main/java/com/example/curs/MainActivity.java
+++ app/src/main/java/com/example/curs/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.curs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.curs.adapter.ProductAdapter;
+import com.example.curs.model.ProductCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ProductAdapter productAdapter;
+    RecyclerView productCatRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<ProductCategory> productCategoryList = new ArrayList<>();
+        productCategoryList.add(new ProductCategory(1, "Popular"));
+        productCategoryList.add(new ProductCategory(2, "All Body Products"));
+        productCategoryList.add(new ProductCategory(3, "Skin Care"));
+        productCategoryList.add(new ProductCategory(4, "Hair"));
+
+        setProductRecycler(productCategoryList);
+    }
+
+    private void setProductRecycler(List<ProductCategory> productCategoryList) {
+        productCatRecycler = findViewById(R.id.cat_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( this, RecyclerView.HORIZONTAL, false);
+        productCatRecycler.setLayoutManager(layoutManager);
+        productAdapter = new ProductAdapter(this, productCategoryList);
+        productCatRecycler.setAdapter(productAdapter);
     }
 }
